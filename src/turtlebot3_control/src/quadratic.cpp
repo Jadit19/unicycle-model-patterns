c74798402f864602c2679f5f0f9010d4b153be44
Rajarshi1001 <rajarshi20@iitk.ac.in>
2022-08-20 18:44:35 +0530

    Added quadratic trajectory

--- src/turtlebot3_control/src/quadratic.cpp
+++ src/turtlebot3_control/src/quadratic.cpp
@@ -1,7 +1,8 @@
-
 #include <iostream>
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include<time.h>
+#include<cstdlib>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 
@@ -11,16 +12,46 @@ double R_MIN;           // Minimum radius of the annular region
 double R_MAX;           // Maximum radius of the annular region
 double INITIAL_ANGLE;   // Initial angle of the bot w.r.t. X-Axis
 int RATE;               // Rate at which the state of the bot will be updated
-
+double r1;
 //! -------- DERIVED PARAMETERS --------
-double M;               // Slope of the linear generating function
-double C;               // y-intercept of the linear generatung function
+double a;               // Slope of the linear generating function
+double b;               // y-intercept of the linear generatung function
 
 //! -------- FINITE STATE MACHINE --------
 enum FSM {              // Finite state machine declaration as enumeration
     ALIGN,              // Responsible for the bot's initial alignment angle
     MOVE                // Responsible for moving the bot according to the control law
 };
+
+void setDerivedParameters(double n){
+    INITIAL_ANGLE *= M_PI / 180;
+    double r1_min;
+    // r1 initilisation for upward facing parabola
+    if(n > 0.25){
+        r1 = rand()/RAND_MAX * R_MIN;
+    }
+    // r1 initilisation for downward parabola
+    else{
+        r1_min = ((R_MIN + R_MAX)*(R_MAX + R_MIN))/4*R_MIN;
+        r1 = (rand()/RAND_MAX + 1)*R_MIN;
+    }
+    a = V * (R_MAX+R_MIN) / ((R_MAX-R_MIN) * ((R_MAX+R_MIN)/2 - r1));
+    b = V*R_MAX + a*r1*R_MAX - a*R_MAX*R_MAX/2;
+
+    std::cout << "--------- Displaying Parameters --------" << std::endl;
+    std::cout << "R_MIN : " << R_MIN << std::endl;
+    std::cout << "R_MAX : " << R_MAX << std::endl;
+    std::cout << "INITIAL_ANGLE : " << INITIAL_ANGLE << std::endl;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "r1 : " << r1 << std::endl;
+    std::cout << "r1_min : " << r1_min << std::endl;
+    std::cout << "velocity : " << V << std::endl;
+}
+
+double f(double r){
+    return a*(r - r1)/r;
+}
 
 //! -------- GLOBAL PARAMETERS --------
 geometry_msgs::Twist cmd_vel = geometry_msgs::Twist();  // Command velocity that is published to the TurtleBot
@@ -31,32 +62,17 @@ double yaw;                 // Yaw angle of the bot
 double angleDiff = 0;       // Angular difference for initial alignment
 FSM state = FSM::ALIGN;     // State the bot currently is in
 
-// Setting the derived parameters
-void setDerivedParameters(){
-    M = (R_MAX+R_MIN)*V/(R_MAX-R_MIN);
-    C = -2*V*(R_MAX*R_MIN)/(R_MAX-R_MIN);
-    INITIAL_ANGLE *= M_PI / 180;
-}
-
-// Linear generating function, g(r) = mr + c
-// => Control function f(r) = m/r
-double f(double r){
-    return (M / r);
-}
-
-// Stop the movement of the bot
 void stopMoving(){
-    cmd_vel.angular.x = 0;
-    cmd_vel.angular.y = 0;
-    cmd_vel.angular.z = 0;
     cmd_vel.linear.x = 0;
     cmd_vel.linear.y = 0;
     cmd_vel.linear.z = 0;
+    cmd_vel.angular.x = 0;
+    cmd_vel.angular.x = 0;
+    cmd_vel.angular.x = 0;
     return;
 }
-
-// Calculating the roll, pitch and yaw of the bot
-void updateRPY(const geometry_msgs::Quaternion& orientation){
+// get roll, pitch, yaw parameters
+void updateRPY(const geometry_msgs::Quaternion &orientation){
     q.setW(orientation.w);
     q.setX(orientation.x);
     q.setY(orientation.y);
@@ -65,32 +81,31 @@ void updateRPY(const geometry_msgs::Quaternion& orientation){
     m.getRPY(roll, pitch, yaw);
     return;
 }
-
-// Calculating u(t) = f(r)
-double calcAlphaDot(const geometry_msgs::Point& position){
+// calculating the angular velocity of robot which is passed as an input to the controller
+double calcAlphaDot(const geometry_msgs::Point &position){
     double r = sqrt(position.x*position.x + position.y*position.y);
     r = std::min(4.0, std::max(2.0, r));
+    // std::cout << f(r) << std::endl;
     return f(r);
 }
-
-// Odometry call-back function
-void odometryCallback(const nav_msgs::OdometryConstPtr& msg){
+// callback function for subscriber
+void odometryCallback(const nav_msgs::OdometryConstPtr &msg){
     stopMoving();
 
-    if (state == FSM::ALIGN){
-        updateRPY(msg->pose.pose.orientation);
+    if(state == FSM::ALIGN){
+        updateRPY(msg-> pose.pose.orientation);
         angleDiff = abs(INITIAL_ANGLE - yaw);
         if (angleDiff > 0.003){
             if (INITIAL_ANGLE > yaw)
                 cmd_vel.angular.z = 0.1 + 0.04*angleDiff;
             else
                 cmd_vel.angular.z = -0.1 - 0.04*angleDiff;
-        } else {
+        }else{
             state = FSM::MOVE;
-            std::cout << std::endl << "Alignment Done!" << std::endl;
-            std::cout << "Moving now.." << std::endl << std::endl;
+            std::cout << "Alignment Done!" << std::endl;
+            std::cout << "Moving turtlebot now!" << std::endl;
         }
-    } else if (state == FSM::MOVE){
+    }else if(state == FSM::MOVE){
         cmd_vel.linear.x = V;
         cmd_vel.angular.z = calcAlphaDot(msg->pose.pose.position);
     }
@@ -99,7 +114,7 @@ void odometryCallback(const nav_msgs::OdometryConstPtr& msg){
 }
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "annular");
+    ros::init(argc, argv, "quadratic");
     ros::NodeHandle nh;
 
     if (!nh.getParam("velocity", V) || !nh.getParam("minimum_radius", R_MIN) || !nh.getParam("maximum_radius", R_MAX) || !nh.getParam("refresh_rate", RATE) || !nh.getParam("initial_angle", INITIAL_ANGLE)){
@@ -107,7 +122,10 @@ int main(int argc, char** argv){
         ROS_ERROR("Exiting now\n");
         return EXIT_FAILURE;
     }
-    setDerivedParameters();
+    srand( (unsigned)time( NULL ) );
+    double n = rand()/RAND_MAX;
+
+    setDerivedParameters(n);
 
     ros::Publisher pub_cmd_vel_ = nh.advertise<geometry_msgs::Twist>("cmd_vel", RATE);
     ros::Subscriber sub_odom_ = nh.subscribe<nav_msgs::Odometry>("odom", RATE, odometryCallback);
@@ -121,3 +139,4 @@ int main(int argc, char** argv){
 
     return EXIT_SUCCESS;
 }
+
