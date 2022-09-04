44fb47e7121f7c2d761c15bab7f4fb2812870997
Jadit19 <jadit19@gmail.com>
2022-09-04 17:52:04 +0530

    move to geometry_msgs::Point

--- src/turtlebot3_control/src/target.cpp
+++ src/turtlebot3_control/src/target.cpp
@@ -2,8 +2,9 @@
 #include <utility>
 #include <ros/ros.h>
 #include <tf/tf.h>
-#include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 
@@ -19,7 +20,7 @@ const double R_MIN = 1.0;
 const double R_MAX = 1.5;
 const double TOLERANCE = 0.001;
 const double INITIAL_ANGLE = -M_PI_2;
-std::vector<std::pair<double, double>> targets;
+std::vector<geometry_msgs::Point> targets;
 
 double M = (R_MAX+R_MIN)*V/(R_MAX-R_MIN);
 double C = (V-M)*R_MAX;
@@ -71,9 +72,9 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg){
 
     if (state == FSM::MOVE){
         cmd_vel.linear.x = 0.05;
-        if (abs(targets[0].first+R_MAX-x) < TOLERANCE){
+        if (abs(targets[0].x+R_MAX-x) < TOLERANCE){
             state = FSM::ALIGN;
-            ROS_WARN("\tMovement done. Aligning now");
+            ROS_WARN("\tMovement done. Aligning now..");
         }
         return;
     }
@@ -95,15 +96,15 @@ void odomCallback(const nav_msgs::OdometryConstPtr& msg){
     
     cmd_vel.linear.x = V;
     r = 1e8;
-    for (std::pair<double, double> target: targets){
-        r = std::min(r, sqrt(pow(target.first-x,2) + pow(target.second-y,2)));
+    for (geometry_msgs::Point target: targets){
+        r = std::min(r, sqrt(pow(target.x-x,2) + pow(target.y-y,2)));
     }
     cmd_vel.angular.z = f(r);
     return;
 }
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "multi");
+    ros::init(argc, argv, "target");
     ros::NodeHandle nh;
 
     ros::Publisher pub_cmd_vel_ = nh.advertise<geometry_msgs::Twist>("cmd_vel", RATE);
@@ -111,10 +112,37 @@ int main(int argc, char** argv){
     ros::Subscriber sub_odom_ = nh.subscribe<nav_msgs::Odometry>("odom", RATE, odomCallback);
     ros::Rate loopRate(RATE);
 
-    targets.push_back(std::make_pair(1, 0));
-    targets.push_back(std::make_pair(0, -1));
-    targets.push_back(std::make_pair(-1, 0));
-    targets.push_back(std::make_pair(0, 1));
+    int noOfTargets, x, y;
+    geometry_msgs::Point p;
+    p.z = 0;
+
+    ROS_WARN("\tEnter the number of targets:");
+    std::cin >> noOfTargets;
+    if (noOfTargets < 2){
+        ROS_ERROR("\tEnter at least 2 targets to switch between.\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int i=1; i<=noOfTargets; i++){
+        std::cout << std::endl;
+        ROS_WARN("\tPosition (x, y) of target #%d:", i);
+        std::cout << "\tEnter x coordinate: ";
+        std::cin >> p.x;
+        std::cout << "\tEnter y coordinate: ";
+        std::cin >> p.y;
+        targets.push_back(p);
+    }
+    std::cout << std::endl;
+    for (int i=0; i<=noOfTargets+1; i++){
+        geometry_msgs::Point c1 = targets[(i)%noOfTargets];
+        geometry_msgs::Point c2 = targets[(i+1)%noOfTargets];
+        double dist = sqrt(pow(c1.x-c2.x,2) + pow(c1.y-c2.y,2));
+        if (dist > (R_MAX+R_MIN)){
+            ROS_ERROR("\tDistance between point %d and %d is %lf > %lf\n", (i%noOfTargets), ((i+1)%noOfTargets), dist, (R_MAX+R_MIN));
+            return EXIT_FAILURE;
+        }
+    }
+    ROS_WARN("\tTargets initialized successfully. Moving now..");
 
     while (ros::ok()){
         ros::spinOnce();
